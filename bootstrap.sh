4ee205645a27857d48d084397aee9d4fde017b61
dmp42 <olivier@webitup.fr>
2012-06-13 17:47:49 +0200

    Updated third-party

--- bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd third-party/Sparkle1.5b6
-# Fix broken includes
-cd Sparkle.framework/Headers/
-ln -s ../Headers Sparkle
-#xcodebuild -sdk macosx10.7 -project src/client/mac/Breakpad.xcodeproj -configuration Release -target All ARCHS=x86_64 ONLY_ACTIVE_ARCH=YES MACOSX_DEPLOYMENT_TARGET=10.6 GCC_VERSION=com.apple.compilers.llvmgcc42
-#xcodebuild -sdk macosx10.7 -project src/client/mac/Breakpad.xcodeproj -configuration Debug -target All ARCHS=x86_64 ONLY_ACTIVE_ARCH=YES MACOSX_DEPLOYMENT_TARGET=10.6 GCC_VERSION=com.apple.compilers.llvmgcc42
